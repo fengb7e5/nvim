85cfa3f8d001280a5d29ba5ddc60af0b054d5b20
fengn7e5 <fengn7e5@163.com>
2023-02-04 14:46:09 +0800

    修改了一些键位

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,17 +11,34 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- ---------- 正常模式 ---------- ---
+
+keymap.set("n", ";", ":")
+keymap.set("n", "S", ":w<CR>")
+keymap.set("n", "Q", ":q!<CR>")
+keymap.set("n", "s", "<nop>")
+
+
+
+keymap.set("n", "H", "5h")
+keymap.set("n", "J", "5j")
+keymap.set("n", "K", "5k")
+keymap.set("n", "L", "5l")
+
 -- 窗口
-keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
-keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "sv", "<C-w>v") -- 水平新增窗口 
+keymap.set("n", "sh", "<C-w>s") -- 垂直新增窗口
 
 -- 取消高亮
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader><CR>", ":nohl<CR>")
 
 -- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
+keymap.set("n", "bj", ":bnext<CR>")
+keymap.set("n", "bk", ":bprevious<CR>")
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+
+
+
